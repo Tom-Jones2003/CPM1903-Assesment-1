18d56b12f9309e74f37dc0ee19e5e82940a63a59
Ed-uol <25253740@students.lincoln.ac.uk>
2022-03-25 18:28:23 +0000

    added error checking for user inputs

--- CMP1903M Assessment 1 Base Code/Input.cs
+++ CMP1903M Assessment 1 Base Code/Input.cs
@@ -9,21 +9,36 @@ namespace CMP1903M_Assessment_1_Base_Code
     public class Input
     {
         //Handles the text input for Assessment 1
-        public string text = "Nothing";
+        public string? text = "";
 
 
         public void userInput()
         {
             Console.WriteLine("Please Enter 'm' for manual text input or 'f' to read from a file");
-            String Mode = Console.ReadLine();
-            if(Mode == "m")
-            {
-                manualTextInput();
-            }
 
-            else if (Mode == "f")
+            while (true)
             {
-                fileTextInput("");
+                bool success = true;
+                var mode = Console.ReadLine();
+
+                switch (mode)
+                {
+                    case "m":
+                        manualTextInput();
+                        break;
+                    case "f":
+                        fileTextInput();
+                        break;
+                    default:
+                        Console.WriteLine("Invalid input mode, please try again:");
+                        success = false;
+                        break;
+                }
+
+                if (success)
+                {
+                    break;
+                }
             }
         }
         
@@ -37,38 +52,73 @@ namespace CMP1903M_Assessment_1_Base_Code
 
             text = Console.ReadLine();
 
-             if(text == null)
+            if(text == null)
             {
-                 text = "";
+                text = "";
             }
 
-            Console.Write(text);
+            Console.Write("You've inputted " + text);
             
             
         }
         
 
         //Method: fileTextInput
-        //Arguments: string (the file path)
-        //Returns: string
-        //Gets text input from a .txt file
-        public void fileTextInput(string fileName)
+        //Gets text input from a .txt file and stores state internally
+        public void fileTextInput()
         {
+            // repeats whole process of attempting to get a valid path, then confirming it
 
             Console.WriteLine("Please Specify File Path: ");
-            fileName = Console.ReadLine();
-            Console.WriteLine($"You entered {fileName}, Is This Correct? (yes/no)");
-            String confirm = Console.ReadLine();
-            if(confirm == "yes")
+            while (true)
             {
-                    text = System.IO.File.ReadAllText(@fileName);
+                string? filePath = null;
+                // process of checking a valid file path
+                while (true)
+                {
+
+                    filePath = Console.ReadLine();
+
+                    if (File.Exists(filePath))
+                    {
+                        break;
+                    } else
+                    {
+                        Console.WriteLine("File doesn't exist, please enter a file that exists: ");
+                    }
+                }
+
+                Console.WriteLine($"You entered {filePath}, Is This Correct? (yes/no)");
+
+                // process of confirming user input
+                while (true)
+                {
+                    string? confirm = Console.ReadLine();
+                    bool retry_path_asking = false;
+
+                    switch (confirm)
+                    {
+                        case "yes":
+                            // get text, exit function
+                            text = File.ReadAllText(filePath);
+                            return;
+                        case "no":
+                            // user will get to attempt the file path entering again
+                            Console.WriteLine("Please write the correct file path: ");
+                            break;
+                        default:
+                            // unknown input, make the user try again
+                            Console.WriteLine("Invalid input, please enter yes or no: ");
+                            retry_path_asking = true;
+                            break;
+                    }
+
+                    if (retry_path_asking)
+                    {
+                        break;
+                    }
+                }
             }
-            else if (confirm == "no")
-            {
-                fileTextInput("");
-            }
-            
-            
         }
 
     }
